424c2c5d946c856ff906b19e4ca2e9494d7d2bea
ellaelisimo <gabriela.barsukova@gmail.com>
2022-11-30 18:55:51 +0200

    commit

--- frontEnd/script.js
+++ frontEnd/script.js
@@ -1,10 +1,19 @@
 const HOSTNAME = "http://localhost:5001/pets";
 
+let orderSelection = "asc";
+let petSelection = ["dog", "cat", "rabbit"];
+
 const getPets = async () => {
-  const response = await fetch(HOSTNAME);
+  const response = await fetch(`http://localhost:5001/pets`);
   const pets = await response.json();
   return pets;
 };
 getPets();
 
-const showPetsTable = () => {};
+const populatePetTable = async () => {
+  const pets = await getPets();
+  const tableElement = document.querySelector("table");
+
+  pets.forEach((pet) => pet, tableElement);
+};
+populatePetTable();
